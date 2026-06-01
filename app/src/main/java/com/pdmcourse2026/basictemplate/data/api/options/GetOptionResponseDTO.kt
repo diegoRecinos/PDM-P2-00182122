0d50101b540566830f3cdd67883a82aa7a72be6c
recinos <00182122@uca.edu.sv>
2026-06-01 17:15:49 -0600

    response dto

--- app/src/main/java/com/pdmcourse2026/basictemplate/data/api/options/GetOptionResponseDTO.kt
+++ app/src/main/java/com/pdmcourse2026/basictemplate/data/api/options/GetOptionResponseDTO.kt
@@ -1,8 +1,12 @@
 package com.pdmcourse2026.basictemplate.data.api.options
 
+import com.pdmcourse2026.basictemplate.data.model.Option
+import kotlinx.serialization.Serializable
 
-//data class GetOptionResponseDTO(
-//
-//){
-//
-//}
+@Serializable
+data class GetOptionResponseDTO(
+    val page: Int? = null,
+    val results: List<OptionDTO>,
+){
+
+}
