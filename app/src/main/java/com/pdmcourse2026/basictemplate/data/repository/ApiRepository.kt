1d1e7e4cecc027aabdf7dc982504093db519e8da
recinos <00182122@uca.edu.sv>
2026-06-01 17:14:53 -0600

    api repository

--- app/src/main/java/com/pdmcourse2026/basictemplate/data/repository/ApiRepository.kt
+++ app/src/main/java/com/pdmcourse2026/basictemplate/data/repository/ApiRepository.kt
@@ -1,20 +1,42 @@
 package com.pdmcourse2026.basictemplate.data.repository
 
+import android.util.Log
+import com.pdmcourse2026.basictemplate.data.api.options.OptionDTO
+import com.pdmcourse2026.basictemplate.data.api.options.toModel
 import io.ktor.client.HttpClient
 import com.pdmcourse2026.basictemplate.data.model.Option
 import io.ktor.client.call.body
 import io.ktor.client.request.get
+import kotlin.collections.map
 
 
 class ApiRepository(private val client: HttpClient) : RepositoryInterface {
 
-    override suspend fun getOptions(): List<Option> {
+    override suspend fun getPosts(): Result<List<Option>> {
 
-        val response: List<Option> = client.get("https://qjcxdvfzyseuvezacxsd.supabase.co/functions/v1/rankeuca/options").body()
+        return try {
+            //intentar peticion
+            val response: List<OptionDTO> = client.get("posts").body()
 
-        return response.map { it.toModel()
+            //success devolvemos lista de posts transformados
+            Result.success(response.map { it.toModel() })
+
+        } catch (e: Exception) {
+            //devolver err
+            Log.e("ApiRepository", "Error fetching posts: ${e.message}", e)
+            Result.failure(e)
+        }
 
     }
+
+
+//    override suspend fun getOptions(): List<Option> {
+//
+//        val response: List<Option> = client.get("https://qjcxdvfzyseuvezacxsd.supabase.co/functions/v1/rankeuca/options").body()
+//
+//        return response.map { it.toModel()
+//
+//    }
 
     override suspend fun createOption(option: Option): Option {
         TODO()
