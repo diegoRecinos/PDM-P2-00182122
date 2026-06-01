2ce0963190e4b36aff6764381944dd654bceced9
recinos <00182122@uca.edu.sv>
2026-06-01 17:12:51 -0600

    api repository

--- app/src/main/java/com/pdmcourse2026/basictemplate/data/api/KtorClient.kt
+++ app/src/main/java/com/pdmcourse2026/basictemplate/data/api/KtorClient.kt
@@ -40,7 +40,7 @@ object KtorClient {
 
     // Configuración aplicada a todas las peticiones
     defaultRequest {
-      url()
+      url(BASE_URL)
       header(HttpHeaders.Accept, "application/json")
       header(HttpHeaders.Authorization, "Bearer $API_KEY")
     }
--- app/src/main/java/com/pdmcourse2026/basictemplate/data/repository/ApiRepository.kt
+++ app/src/main/java/com/pdmcourse2026/basictemplate/data/repository/ApiRepository.kt
@@ -2,12 +2,18 @@ package com.pdmcourse2026.basictemplate.data.repository
 
 import io.ktor.client.HttpClient
 import com.pdmcourse2026.basictemplate.data.model.Option
+import io.ktor.client.call.body
+import io.ktor.client.request.get
 
 
 class ApiRepository(private val client: HttpClient) : RepositoryInterface {
 
     override suspend fun getOptions(): List<Option> {
-        TODO()
+
+        val response: List<Option> = client.get("https://qjcxdvfzyseuvezacxsd.supabase.co/functions/v1/rankeuca/options").body()
+
+        return response.map { it.toModel()
+
     }
 
     override suspend fun createOption(option: Option): Option {
